46dc062324cb1f5e6940437bcf63883d6bc49e4c
j-blalock <justin.m.blalock@gmail.com>
2020-10-27 16:30:12 -0500

    day 2 additions

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('finches/', views.finches_index, name='finches_index')
+    path('finches/', views.finches_index, name='finches_index'),
+    path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
+    path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import finches
+from django.shortcuts import render, redirect
+from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,28 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
+
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+
+    feeding_form = FeedingForm()
+
+    return render(request, 'finches/detail.html', {
+        'finch': finch,
+        'feeding_form': feeding_form
+    })
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+
+    # if form is valid
+    if form.is_valid():
+        #submit the form
+        new_form = form.save(commit=False)
+        new_form.finch_id = finch_id
+        new_form.save()
+
+    return redirect('detail', finch_id=finch_id)
